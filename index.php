00aa03a67ec4bfcd6da8d45523eab8f91bd5f400
darknawg63 <darknawg63@gmail.com>
2015-02-10 20:53:11 +0100

    Database (Part 7/23)

--- index.php
+++ index.php
@@ -2,4 +2,13 @@
 
 require_once 'core/init.php';
 
-echo Config::get('mysql/host');
+$user = DB::getInstance()->query("SELECT username 
+                         FROM users
+                         WHERE username = ?",
+                         array('alex'));
+
+if($user->error()) {
+    echo 'No user';
+} else {
+    echo 'OK';
+}
