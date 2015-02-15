cb831cbee4b9c937ba06d42f5af186041697e103
darknawg63 <darknawg63@gmail.com>
2015-02-15 22:20:02 +0100

    Form Validation (Part 11/23)

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 
 require_once 'core/init.php';
 
-$user = DB::getInstance()->update('users', 2, array(
+$user = DB::getInstance()->update('users', 3, array(
     'password' => 'newpassword',
     'name'     => ''
 ));
--- register.php
+++ register.php
@@ -3,7 +3,39 @@
 require_once 'core/init.php';
 
 if(Input::exists()) {
-  echo Input::get('username');
+  
+  $validate = new Validate();
+  $validation = $validate->check($_POST, array(
+
+    'username'       => array(
+        'required' => true,
+        'min'      => 2,
+        'max'      => 20,
+        'unique'   => 'users'
+      ),
+    'password'       => array(
+        'required' => true,
+        'min'      => 6
+      ),
+    'password_again' => array(
+        'required' => true,
+        'matches'  => 'password'
+      ),
+    'name'           => array(
+        'required' => true,
+        'min'      => 2,
+        'max'      => 50
+      )
+  ));
+
+  if($validation->passed()) {
+    echo 'Passed';
+  } else {
+      
+      foreach($validation->errors() as $error) {
+        echo $error, '<br>';
+      }
+  }
 }
 
 ?>
@@ -11,7 +43,7 @@ if(Input::exists()) {
 <form action="" method="post">
   <div class="field">
     <label for="username">Username</label>
-    <input type="text" name="username" id="username" value="" autocomplete="off">   
+    <input type="text" name="username" id="username" value="<?php echo escape(Input::get('username')); ?>" autocomplete="off">   
   </div>
 
   <div class="field">
@@ -26,7 +58,7 @@ if(Input::exists()) {
 
   <div class="field">
     <label for="name">Full name</label>
-    <input type="text" name="name" id="name" value="">
+    <input type="text" name="name" id="name" value="<?php echo escape(Input::get('name')); ?>">
   </div>
 
   <input type="submit" value="Register">
