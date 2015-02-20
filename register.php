05eac79e62596d2d7024abf74b7c108baf146914
darknawg63 <darknawg63@gmail.com>
2015-02-20 16:07:07 +0100

    Redirecting (Part 15/23)

--- register.php
+++ register.php
@@ -47,7 +47,8 @@ if(Input::exists()) {
         ));
 
         Session::flash('home', 'You have been registered and can now log in!');
-        header('Location: index.php');
+        Redirect::to(index.php);
+
       } catch(Exception $e) {
 
           die($e->getMessage());
